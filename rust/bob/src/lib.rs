737d14ec0c6d919c9c32530ca231aa0bd2d102fa
Demare <elias.demaertelaere@gmail.com>
2018-09-15 18:33:28 +0200

    cleaning up code

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,21 +1,19 @@
 pub fn reply(message: &str) -> &str {
     let message = message.trim();
     let no_alphabet = message.chars().all(|c| !c.is_alphabetic());
-    
+
     match message {
         _ if message.is_empty() => "Fine. Be that way!",
         _ if message.chars().last() == Some('?') => {
             match message {
-                _ if no_alphabet => "Sure.",
-                _ if message.to_uppercase() == message => "Calm down, I know what I'm doing!",
+                _ if message.to_uppercase() == message && !no_alphabet => "Calm down, I know what I'm doing!",
                 _ => "Sure.",
             }
         
         },
         _ => {
             match message {
-                _ if no_alphabet => "Whatever.",
-                _ if message.to_uppercase() == message => "Whoa, chill out!",
+                _ if message.to_uppercase() == message && !no_alphabet => "Whoa, chill out!",
                 _ => "Whatever.",
             }
         }
