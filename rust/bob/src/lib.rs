0a337090e5176d404e14c045994041889c22dd96
Demare <elias.demaertelaere@gmail.com>
2018-09-15 18:20:59 +0200

    cleaning up code

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,26 +1,23 @@
 pub fn reply(message: &str) -> &str {
     let message = message.trim();
-    if message.is_empty() {
-        "Fine. Be that way!"
-    } else if message.chars().last() == Some('?') {
-        if message.chars().all(|c| !c.is_alphabetic()) {
-            "Sure." 
-        } else {
-            if message.to_uppercase() == message {
-                "Calm down, I know what I'm doing!"
-            } else {
-                "Sure." 
-            }          
-        }
-    } else {
-        if message.chars().all(|c| !c.is_alphabetic()) {
-            "Whatever." 
-        } else {
-            if message.to_uppercase() == message {
-                "Whoa, chill out!"
-            } else {
-                "Whatever." 
-            }          
+    let no_alphabet = message.chars().all(|c| !c.is_alphabetic());
+    
+    match message {
+        _ if message.is_empty() => "Fine. Be that way!",
+        _ if message.chars().last() == Some('?') => {
+            match message {
+                _ if no_alphabet => "Sure.",
+                _ if message.to_uppercase() == message => "Calm down, I know what I'm doing!",
+                _ => "Sure.",
+            }
+        
+        },
+        _ => {
+            match message {
+                _ if no_alphabet => "Whatever.",
+                _ if message.to_uppercase() == message => "Whoa, chill out!",
+                _ => "Whatever.",
+            }
         }
     }
 }
