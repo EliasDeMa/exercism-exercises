d1138699b809a687f2a11753dfd779aae227902f
Demare <elias.demaertelaere@gmail.com>
2018-09-16 20:44:34 +0200

    optimize saddle points exercise

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,12 +1,10 @@
-use std::iter;
-
 pub fn find() -> Option<u32> {
     let sum = 1000;
     (2..sum)
         .flat_map(|c| {
             let a = sum - c;
-            (1..a).zip(iter::repeat(c))
-        }).map(|(a, c)| (a, sum - c - a, c))
+            (1..a).map(move |a| (a, sum - c - a, c))
+        })
         .find(|&(a, b, c)| a * a + b * b == c * c)
         .map(|(a, b, c)| a * b * c)
 }
--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,21 +1,25 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut output: Vec<(usize, usize)> = Vec::new();
 
+    let max_values_row: Vec<u64> = input.iter()
+                                    .map(|row| find_row_max(row))
+                                    .collect();
+    let min_values_column: Vec<u64> = (0..input[0].len()) //input[0].len() = amount of columns
+                                    .map(|column| find_column_min(input, column))
+                                    .collect();
+    
     // use enumerate to make it easier to enter index if necessary
     for (i, row) in input.iter().enumerate() {
-        let max = find_row_max(row);
-
-        for (j, element) in row.iter().enumerate() {
-            let min = find_column_min(input, j);
-
+        for (j, element) in row.iter().enumerate() {     
             // check if element is both the max value in the row and
             // the min value in the column
-            if element == &max && element == &min {
+            // use i and j to check with respective values in precalculated vectors
+            if element == &max_values_row[i] && element == &min_values_column[j] {
                 output.push((i, j));
             }
         }
     }
-    
+
     output
 }
 
