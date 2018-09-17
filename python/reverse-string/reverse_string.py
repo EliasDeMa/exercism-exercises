41fa7d255e0be88abfe7bb5ec1a9a548b2cdf86b
Demare <elias.demaertelaere@gmail.com>
2018-09-17 11:52:34 +0200

    cleaning up

--- python/reverse-string/reverse_string.py
+++
@@ -1,5 +0,0 @@
-def reverse(text):
-    if len(text) == 0:
-        return ""
-    else:
-        return text[-1] + reverse(text[:-1])
--- python/rna-transcription/rna_transcription.py
+++
@@ -1,8 +0,0 @@
-def to_rna(dna_strand):
-    DNA = ['G', 'C', 'T', 'A']
-    RNA = ['C', 'G', 'A', 'U']
-    result = ""
-    for letter in dna_strand:
-       result += RNA[DNA.index(letter)]
-    return result
-
